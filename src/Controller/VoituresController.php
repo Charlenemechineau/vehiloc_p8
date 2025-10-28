e8a09307fdce9e19c0c356d8d3b5dc4271cf4faa
Charlene Mechineau <charlene.mechineau@gmail.com>
2025-10-28 22:43:23 +0100

    Étape 4 : ajout page de détail voiture et lien dynamique

--- src/Controller/VoituresController.php
+++ src/Controller/VoituresController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-// Permet d'accéder aux voitures dans la base
+// Permet d'accéder aux voitures dans la base//
 use App\Repository\VoitureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,13 +13,31 @@ class VoituresController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function accueil(VoitureRepository $voitureRepository): Response
     {
-        // 1. Récupérer la liste des voitures depuis la base
+        // 1. Récupérer la liste des voitures depuis la base//
         $voitures = $voitureRepository->findAll();
 
-        // 2. Afficher le template accueil.html.twig
-        // 3. Lui passer la liste des voitures
+        // 2. Afficher le template accueil.html.twig//
+        // 3. Lui passer la liste des voitures//
         return $this->render('accueil.html.twig', [
             'voitures' => $voitures,
         ]);
     }
+
+    #[Route('/voiture/{id}', name: 'app_voiture_detail')]
+    public function detail(int $id, VoitureRepository $voitureRepository): Response
+    {
+        // Récupérer la voiture par son id dans l'URL//
+        $voiture = $voitureRepository->find($id);
+
+        // Si pas trouvée -> retour à l'accueil//
+        if (!$voiture) {
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        // Sinon, afficher le template détail//
+        return $this->render('voitures/detail.html.twig', [
+            'voiture' => $voiture,
+        ]);
+    }
+
 }
